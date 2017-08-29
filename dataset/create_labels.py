4188aa714da405fe1104c776301b1344fd1f6117
Tomáš Souček <soucek.gns@gmail.com>
2017-08-29 09:34:42 +0200

    labels file generation

---
+++ dataset/create_labels.py
@@ -0,0 +1,81 @@
+from nltk.corpus import wordnet as wn
+# https://stackoverflow.com/questions/8077641/how-to-get-the-wordnet-synset-given-an-offset-id
+
+
+class Synset:
+    def __init__(self, wn_id, names, desc):
+        self.wn_id = wn_id
+        self.names = names
+        self.name = '#'.join(names).replace('_', ' ')
+        self.desc = desc
+
+
+class Hypernym:
+    def __init__(self, synset):
+        self.hyponyms = []
+        self.synset = synset
+
+
+class ImageClass:
+    def __init__(self, local_id, synset):
+        self.hypernyms = []
+        self.hyponyms = []
+        self.local_id = local_id
+        self.synset = synset
+
+
+def load_synsets():
+    synsets = []
+    with open('synset_ids.txt', 'r') as f:
+        synset_id = f.readline()
+        while synset_id != '':
+            synset_id = int(synset_id[1:])
+            synset = wn._synset_from_pos_and_offset('n', synset_id)
+
+            synsets.append(synset)
+            synset_id = f.readline()
+    return synsets
+
+
+def get_hypernyms():
+    classes = {}
+    synsets = load_synsets()
+
+    for synset in synsets:
+        new_class = ImageClass(0, Synset(synset.offset(), synset.lemma_names(), synset.definition()))
+
+        if synset.offset() in classes:
+            new_class.hyponyms = classes[synset.offset()].hyponyms
+        classes[synset.offset()] = new_class
+
+        for hm in synset.hypernyms():
+            if hm.offset() in classes:
+                classes[hm.offset()].hyponyms.append(classes[synset.offset()])
+            else:
+                classes[hm.offset()] = Hypernym(Synset(hm.offset(), hm.lemma_names(), hm.definition()))
+                classes[hm.offset()].hyponyms.append(classes[synset.offset()])
+
+            classes[synset.offset()].hypernyms.append(classes[hm.offset()])
+
+    return classes
+
+
+def create_labels():
+    classes = get_hypernyms()
+
+    with open('classes.labels', 'w') as f:
+        i = 0
+        for k in sorted(classes):
+            c = classes[k]
+            if type(c) is Hypernym:
+                f.write('H~{:08d}~{}~{}~~{}\n'.format(c.synset.wn_id, c.synset.name,
+                                                      '#'.join([str(x.synset.wn_id) for x in c.hyponyms]),
+                                                      c.synset.desc))
+            else:
+                f.write('{:d}~{:08d}~{}~{}~{}~{}\n'.format(i, c.synset.wn_id, c.synset.name,
+                                                           '#'.join([str(x.synset.wn_id) for x in c.hyponyms]),
+                                                           '#'.join([str(x.synset.wn_id) for x in c.hypernyms]),
+                                                           c.synset.desc))
+                i += 1
+
+create_labels()
