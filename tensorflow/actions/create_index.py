d4cd1bf58eebb9d4d90550693d0b5968e8162e1f
Tomáš Souček <soucek.gns@gmail.com>
2017-09-14 18:25:29 +0200

    prepare classified images for web check

--- tensorflow/actions/create_index.py
+++ tensorflow/actions/create_index.py
@@ -1,3 +1,4 @@
+import os
 import struct
 import argparse
 
@@ -47,6 +48,41 @@ def create_index_file(pseudo_index_filename, index_filename, num_classes_per_ima
 
     file.close()
 
+
+def prepare_for_web(pseudo_index_filename, destination, num_classes_per_image, max_photos=48, min_prob=0):
+    classes = get_class_representatives(pseudo_index_filename, num_classes_per_image)
+    sorted_classes = sorted(classes)
+
+    sql = open(os.path.join(destination, 'top_images_by_class.sql'), 'w')
+    sh = open(os.path.join(destination, 'top_images_by_class-copy_to_web.sh'), 'w')
+
+    sh.write('LOCATION=\nDESTINATION=\n')
+    sql.write("INSERT INTO `comparison` (`image_name`, `class`, `probability`, `dataset`) VALUES\n")
+
+    first = True
+    for key in sorted_classes:
+        photos = classes[key]
+        photos.sort(key=lambda tup: -tup[1])
+
+        sh.write('mkdir ${{DESTINATION}}/{}\n'.format(key))
+        for index, (photo_id, photo_val) in enumerate(photos):
+            if index >= max_photos:
+                break
+            if photo_val < min_prob:
+                break
+
+            if not first:
+                sql.write(',\n')
+            else: first = False
+
+            sh.write('cp ${{LOCATION}}/{0}.jpg ${{DESTINATION}}/{1}/{0}.jpg\n'.format(photo_id, key))
+            sql.write("('{}.jpg', '{}', '{}', '0')".format(photo_id, key, photo_val))
+
+    sql.write(';\n')
+    sql.close()
+    sh.close()
+
+#prepare_for_web('bin/files.pseudo-index', 'bin', 10, max_photos=3, min_prob=0)
 
 # py actions/create_index.py --pseudo_index_filename="files.pseudo-index" --index_filename="files.index" --num_classes=2
 if __name__ == '__main__':
