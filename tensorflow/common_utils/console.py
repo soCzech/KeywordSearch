99e24764d7f9e83eef1897444e6a52a1ecf30f33
Tomáš Souček <soucek.gns@gmail.com>
2018-01-27 23:32:00 +0100

    estimated end time

--- tensorflow/common_utils/console.py
+++ tensorflow/common_utils/console.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+import math
 
 
 class _Singleton(type):
@@ -21,31 +23,39 @@ class ProgressTracker(Singleton):
         self._total = 0
         self._digits = 0
 
+        self._remaining_time = "?"
+        self._last_current = 0
+        self._last_time = None
+
     def reset(self, total):
         self._current = 0
         self._total = total
         self._digits = len(str(total))
+
+        self._remaining_time = "?"
+        self._last_current = 0
+        self._last_time = None
 
         self._calculate_bar_width()
         self._draw()
 
     def _calculate_bar_width(self):
         (self._tw, self._th) = ProgressTracker._terminal_size()
-        self._bar_width = self._tw - (2 * self._digits + 3 + 5)
+        self._bar_width = self._tw - (2 * self._digits + 3 + 5 + 5 + len(self._remaining_time))
 
     def _draw(self):
         if self._total == 0:
             return
         self._clear()
 
-        print("%-*s%3d%% (%*d/%*d)" % (
-            self._bar_width,
+        print('{0:-<{1}}{2:3d}% ({3:{5}d}/{4:{5}d}) ETA {6}'.format(
             "=" * round(self._bar_width * self._current / self._total),
-            round(self._current / self._total * 100),
-            self._digits,
+            self._bar_width,
+            round(math.floor(self._current / self._total * 100)),
             self._current,
+            self._total,
             self._digits,
-            self._total
+            self._remaining_time
         ), end='\r')
 
     def _clear(self):
@@ -57,9 +67,34 @@ class ProgressTracker(Singleton):
 
     def set_progress(self, current):
         self._current = current
+        if self._last_time is None or (datetime.datetime.now() - self._last_time).seconds > 1:
+            self._update_time()
+
         self._draw()
         if self._current == self._total:
             self.reset(0)
+
+    def _update_time(self):
+        if self._last_time is None:
+            self._last_time = datetime.datetime.now()
+            self._remaining_time = "?"
+        else:
+            diff = datetime.datetime.now() - self._last_time
+            self._last_time = datetime.datetime.now()
+            diff = (diff.seconds * 1E6 + diff.microseconds) /\
+                   (self._current - self._last_current) * (self._total - self._current) / 1E6
+            self._last_current = self._current
+
+            if diff > 3600:
+                h = round(diff//3600)
+                m = round((diff - h*3600)/60)
+                self._remaining_time = "{0:d}h {1:d}m".format(h, m)
+            elif diff > 60:
+                m = round(diff // 60)
+                s = round((diff - m * 60))
+                self._remaining_time = "{0:d}m {1:d}s".format(m, s)
+            else:
+                self._remaining_time = "{0:d}s".format(round(diff))
 
     def increment(self, val=1):
         self.set_progress(self._current + val)
